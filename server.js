603f9baee6ccd6f46b1b3fc85c012624a1fce702
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-07-26 14:36:16 +0100

    complete

--- server.js
+++ server.js
@@ -10,26 +10,23 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8092;
 
-app.get <
-  ("/",
-  (request, response) => {
-    response.status(200).json("the test is working");
-  });
+app.get("/", (request, response) => {
+  response.status(200).json("the test is working");
+});
 
 app.get("/photos", async (request, response) => {
   const API = `https://api.unsplash.com/search/photos/?client_id=${process.env.ACCESS_KEY}&query=goat`;
   const res = await axios.get(API);
   console.log(res.data.results[0]);
-  response.status(200).json("hello");
-  const photos = res.data.results.map((photo)=>{
-    return{
-        id: photo.id,
-        img_url: photo.urls.regular,
-        original_image: photo.links.self,
-        photographer: photo.user.name
-
-    }
-  }
+  const photos = res.data.results.map((photo) => {
+    return {
+      id: photo.id,
+      img_url: photo.urls.regular,
+      original_image: photo.links.self,
+      photographer: photo.user.name,
+    };
+  });
+  response.status(200).json(photos);
 });
 
 app.listen(PORT, () => console.log(`the app is listening on port ${PORT}`));
